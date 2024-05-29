e9578084b513b7c6151b3f632a0611bcb22b109a
angelargd8 <100092195+angelargd8@users.noreply.github.com>
2024-05-29 15:08:11 -0600

    Todos los planetas se mueven en torno al sol y estrellas estaticas

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,32 @@
+"use client";
 import styles from "./page.module.css";
+import stylesPlanet from "./planetas.module.css";
+import React, { useEffect, useState } from "react";
 export default function Home() {
+
+  const [starsStyle, setStarsStyle] = useState({});
+
+  useEffect(() => {
+    const layer = [];
+    const totalStars = 200;
+    for(let i = 0; i < totalStars; i++) {
+      const x = Math.floor(Math.random() * 100);
+      const y = Math.floor(Math.random() * 100);
+      layer.push(`${x}vw ${y}vh 0 white` );
+    }
+    setStarsStyle({
+      '--space-layer': layer.join(','),
+      '--size': '2px',
+    });
+
+  
+  }, [])
 
   return (
     <main className={styles.main}>
+      <div className={styles.starsContainer} style= {starsStyle}></div>
       <div className={styles.center}>
-        
+      
       {/*sistema solar*/}
       <div className={styles.sistemaSolar}>
           {/*sol*/}
@@ -23,8 +45,8 @@ export default function Home() {
             }     
             {/*planetas*/}   
             {
-              Array.from({ length: 8 }).map((_, i) => (
-                  <div className={`${styles.planeta} ${styles[`planeta${i + 1}`]}`}></div>
+              Array.from({ length: 9 }).map((_, i) => (
+                  <div className={`${stylesPlanet.planeta} ${stylesPlanet[`planeta${i + 1}`]}`}></div>
 
               ))
             }
