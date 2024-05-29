df5428597ccecd735616b04424ff27a5a1fc1982
angelargd8 <100092195+angelargd8@users.noreply.github.com>
2024-05-29 13:32:56 -0600

    se mueve la tierra

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,4 @@
 import styles from "./page.module.css";
-
 export default function Home() {
 
   return (
@@ -8,29 +7,32 @@ export default function Home() {
         
       {/*sistema solar*/}
       <div className={styles.sistemaSolar}>
-        {/*sol*/}
-        <div className={styles.mercurio}></div>
-        <div className={styles.solContainer}>
-          <div className={styles.solTop}></div>
-        </div>
-        <div className={styles.solBottom}></div>
+          {/*sol*/}
+          <div className={styles.mercurio}></div>
+          <div className={styles.solContainer}>
+            <div className={styles.solTop}></div>
+          </div>
+          <div className={styles.solBottom}></div>
         
-          {/*orbitas*/}
-          {
-            Array.from({ length: 8 }).map((_, i) => (
-              <div className={`${styles.orbita} ${styles[`orbita${i + 1}`]}`}>
-                {/*planetas<div className={styles.planeta}></div>
-                <div className={`${styles.planeta} ${styles[`planeta${i + 1}`]}`}></div>*/}
-                <div className={`${styles.planeta} ${styles[`planeta${i + 1}`]}`}></div>
-              </div>
-              
-            ))
-          }
-          
-        
+          <div className={styles.ContainerOrbitaPlanetas}>
+            {/*orbitas*/}
+            {
+              Array.from({ length: 8 }).map((_, i) => (
+                <div className={`${styles.orbita} ${styles[`orbita${i + 1}`]}`}></div>
+              ))
+            }     
+            {/*planetas*/}   
+            {
+              Array.from({ length: 8 }).map((_, i) => (
+                  <div className={`${styles.planeta} ${styles[`planeta${i + 1}`]}`}></div>
+
+              ))
+            }
+
+          </div>
+            
       </div>
-        
-        
+
 
       </div>
     </main>
