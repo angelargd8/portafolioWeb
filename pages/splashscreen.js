8f2d73d54adb84540be8ab262f5ee5fdf500a07b
angelargd8 <100092195+angelargd8@users.noreply.github.com>
2024-05-30 07:44:51 -0600

    splashscreen terminada

--- pages/splashscreen.js
+++ pages/splashscreen.js
@@ -1,52 +1,58 @@
 import stylesSplash from "../styles/splash.module.css";
 import React, { useEffect, useState } from "react";
 export default function SplashScreen() {
-    {/*
-    <div 
-      style={{ 
-        position: 'fixed', 
-        top: 0, 
-        left: 0, 
-        right: 0, 
-        bottom: 0, 
-        display: 'flex', 
-        alignItems: 'center', 
-        justifyContent: 'center', 
-        backgroundColor: 'black' 
-        }}>
-        <h1>Cargando...</h1>
-      </div>
-    */}
-    const [bubbles, setbubbles] = useState([]);
+    
+    const [starsStyle, setStarsStyle] = useState({});
 
-    useEffect(() => {
-        const newBubbles = [];
-        const totalBubbles = 100;
-        for(let i = 0; i < totalBubbles; i++) {
-            const size = Math.floor(Math.random() * 30);
-            const x = Math.floor(Math.random() * 100);
-            const y = Math.floor(Math.random() * 100);
-            const z = Math.floor(Math.random() * 2000);
-            const hue = Math.floor(Math.random() * 360);
-            const bubble ={
-                size: `${size}px`,
-                transform: `translate3d(${x}px, ${y}px, ${z}px)`,
-                backgroundColor: `hsl(${hue}, 70%, 50%)`,
-                animationDelay: `${i * 0.2}s`,
-            }
-            newBubbles.push(bubble); 
+      useEffect(() => {
+        const layer = [];
+        const totalStars = 700;
+        for(let i = 0; i < totalStars; i++) {
+          const x = Math.floor(Math.random() * 100);
+          const y = Math.floor(Math.random() * 100);
+          const hue = Math.floor(Math.random() * 360);
+          const r = Math.floor(Math.random() * 255);
+          const g = Math.floor(Math.random() * 255);
+          const b = Math.floor(Math.random() * 255);
+          const opacity = Math.random();
+          layer.push(`${x}vw ${y}vh 0 rgba(${r}, ${g}, ${b}, ${opacity})` );
         }
-        setbubbles( newBubbles );
+        setStarsStyle({
+          '--space-layer': layer.join(','),
+          '--size': '3px',
+          
+        });
+    
+      
       }, [])
+    
 
     return (
         <div>
         
         <div className={stylesSplash.bubbleContainer}>
-            {bubbles.map((bubble, index) => (
-                <div className={stylesSplash.bubble} key={index} style={bubble}></div>
-            ))}
+        {<div className={stylesSplash.starsContainer} style= {starsStyle}></div>
+           }
+          
+        {/*<div className={stylesSplash.starsContainer} style={{...starsStyle, '--space-layer': starsStyle['--space-layer'], '--size': starsStyle['--size']}}></div>
+        */}
         </div>
+
+        <div 
+        style={{ 
+          position: 'fixed', 
+          top: 0, 
+          left: 0, 
+          right: 0, 
+          bottom: 0, 
+          display: 'flex', 
+          alignItems: 'center', 
+          justifyContent: 'center', 
+          backgroundColor: 'black',
+          color: 'white '
+        }}>
+        <h1>Cargando...</h1>
+      </div>
         
         </div>
     
